b1cafda3230c4abe4863b9d5ae4e2acd63d0b7c7
Abhijith Mahipal <abhijithm@valorem.com>
2020-06-28 21:57:28 +0530

    installed dep,added env,added components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { ReposComponent } from './components/repos/repos.component';
+import { UserCardComponent } from './components/user-card/user-card.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ReposComponent,
+    UserCardComponent,
+    HomeComponent,
+    PagenotfoundComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: 'AIzaSyCbI_N7klIv1bU-M5lMEhHzLjTU2vWUhDo',
+    authDomain: 'angular-login-d16a8.firebaseapp.com',
+    databaseURL: 'https://angular-login-d16a8.firebaseio.com',
+    projectId: 'angular-login-d16a8',
+    storageBucket: 'angular-login-d16a8.appspot.com',
+    messagingSenderId: '947428261563',
+    appId: '1:947428261563:web:b132e44113cc60c463339e'
+  }
 };
 
 /*
