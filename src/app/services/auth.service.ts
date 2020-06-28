c849aa03d917f70b58758dc893703c424ee1c0af
Abhijith Mahipal <abhijithm@valorem.com>
2020-06-28 22:32:56 +0530

    services and calls added

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private auth: AngularFireAuth) { }
+
+  signUp(email: string, password: string) {
+    return this.auth.createUserWithEmailAndPassword(email, password);
+  }
+  singIn(email: string, password: string) {
+    return this.auth.signInWithEmailAndPassword(email, password);
+  }
+  getUser(){
+    return this.auth.authState;
+  }
+  signOut(){
+    this.auth.signOut();
+  }
 }
--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
+  // https://api.github.com/users/abhijithmahipal
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getUserDetails(userName: string){
+    return this.http.get(`https://api.github.com/users/${userName}`);
+  }
+  getRepos(repoUrl: string){
+    return this.http.get(repoUrl);
+  }
 }
