249cfec4832ed429520f5af7a2e1cb771bd1589d
Abhijith Mahipal <abhijithm@valorem.com>
2020-07-04 17:21:17 +0530

    bug fixed, signout added

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -9,16 +9,29 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  email: any;
 
   constructor(
     private auth: AuthService,
     private toastr: ToastrService,
     private router: Router
   ) {
-    //auth.getUser.subscribe()
+    this.auth.getUser().subscribe((user: firebase.User) => {
+      this.email = user?.email;
+    });
   }
 
   ngOnInit(): void {
   }
 
+  async handleSignOut() {
+    try {
+      const response = await this.auth.signOut();
+      this.toastr.info('Login again to continue');
+      this.email = null;
+      this.router.navigateByUrl('/signin');
+    } catch (error) {
+      this.toastr.error('Something is wrong');
+    }
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,16 +9,16 @@ export class AuthService {
 
   constructor(private auth: AngularFireAuth) { }
 
-  signUp(email: string, password: string) {
+  signUp(email: string, password: string): Promise<firebase.auth.UserCredential> {
     return this.auth.createUserWithEmailAndPassword(email, password);
   }
-  singIn(email: string, password: string) {
+  singIn(email: string, password: string): Promise<firebase.auth.UserCredential> {
     return this.auth.signInWithEmailAndPassword(email, password);
   }
-  getUser(){
+  getUser(): Observable<firebase.User> {
     return this.auth.authState;
   }
-  signOut(){
+  signOut() {
     this.auth.signOut();
   }
 }
