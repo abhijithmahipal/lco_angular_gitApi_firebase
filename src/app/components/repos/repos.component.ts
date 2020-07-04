eed0741f74491271ca3973d961bb8e19e3fb6256
Abhijith Mahipal <abhijithm@valorem.com>
2020-07-04 18:29:09 +0530

    gitrepo list added

--- src/app/components/repos/repos.component.ts
+++ src/app/components/repos/repos.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, ChangeDetectorRef } from '@angular/core';
+import { GithubService } from 'src/app/services/github.service';
 
 @Component({
   selector: 'app-repos',
   templateUrl: './repos.component.html',
   styleUrls: ['./repos.component.scss']
 })
-export class ReposComponent implements OnInit {
-
-  constructor() { }
+export class ReposComponent implements OnInit, OnChanges {
+  @Input() repoUrl: string;
+  repos = [];
+  constructor(private githubService: GithubService, private ref: ChangeDetectorRef) { }
 
   ngOnInit(): void {
   }
-
+  ngOnChanges(): void {
+    if (this.repoUrl) {
+      this.githubService.getRepos(this.repoUrl).subscribe(
+        (repos: []) => {
+          this.repos = repos;
+          this.ref.detectChanges();
+        },
+        (err) => {
+          console.log(err);
+        });
+    }
+  }
 }
