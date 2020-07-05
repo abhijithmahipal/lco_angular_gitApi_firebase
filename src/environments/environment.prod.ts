7adadb77ba1acae7a0701091d4e753275ed6895a
Abhijith Mahipal <abhijithm@valorem.com>
2020-07-05 09:07:16 +0530

    removed key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,3 @@
 export const environment = {
-  production: true,
-  firebase : {
-    apiKey: 'AIzaSyCbI_N7klIv1bU-M5lMEhHzLjTU2vWUhDo',
-    authDomain: 'angular-login-d16a8.firebaseapp.com',
-    databaseURL: 'https://angular-login-d16a8.firebaseio.com',
-    projectId: 'angular-login-d16a8',
-    storageBucket: 'angular-login-d16a8.appspot.com',
-    messagingSenderId: '947428261563',
-    appId: '1:947428261563:web:b132e44113cc60c463339e'
-  }
+  production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,6 @@
 
 export const environment = {
   production: false,
-  firebase : {
-    apiKey: 'AIzaSyCbI_N7klIv1bU-M5lMEhHzLjTU2vWUhDo',
-    authDomain: 'angular-login-d16a8.firebaseapp.com',
-    databaseURL: 'https://angular-login-d16a8.firebaseio.com',
-    projectId: 'angular-login-d16a8',
-    storageBucket: 'angular-login-d16a8.appspot.com',
-    messagingSenderId: '947428261563',
-    appId: '1:947428261563:web:b132e44113cc60c463339e'
-  }
 };
 
 /*
